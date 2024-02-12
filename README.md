01b98c0a30fc611be7423cde697260c43a755f1f
Edmund-Frio <edmundfrio@gmail.com>
2024-02-12 10:14:59 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Automated KACE login using mouse clicks
---
+++ login.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+pyautogui.click(1500, 1400)
+pyautogui.click(800, 160, duration=2.5)
+pyautogui.click(1300, 1150, duration=2.25)
+pyautogui.click(500, 500, duration=1)
+pyautogui.click(1780, 400, duration=0.5)
+pyautogui.moveTo(1780, 600, duration=0.5)
+pyautogui.moveTo(1600, duration=0.5)
+pyautogui.click(1600, 700, duration=0.5)
